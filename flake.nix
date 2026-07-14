4a5ca51ecf79ee654a1eb8917588a65b8e26f211
Kori <daniilignatev290@proton.me>
2026-07-14 16:31:56 +0300

    Add custom shell tools and wizards bundle

--- flake.nix
+++ flake.nix
@@ -1,1 +1,23 @@
+{
+  description = "Kori's custom shell tools and wizards bundle";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      
+      # Импортируем ОДИН единственный файл-оркестратор
+      maestro = import ./commands-nix/maestro_commands.nix { inherit pkgs; };
+    in
+    {
+      packages.${system} = rec {
+        # Экспортируем бандл, который собрал маэстро
+        icebook-aliasses-pkgs = maestro.icebook-aliasses-pkgs;
+
+        # По умолчанию отдаем его же
+        default = icebook-aliasses-pkgs;
+      };
+    };
+}
