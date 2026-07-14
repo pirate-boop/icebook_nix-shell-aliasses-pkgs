45a8b0aaba0221158bf08d37502778027c0e9055
Kori <daniilignatev290@proton.me>
2026-07-14 17:10:33 +0300

    Import niri-commands in maestro_aliasesses.nix
    Added import for niri-commands in maestro_aliasesses.nix.

--- commands/maestro_aliasesses.nix
+++ commands/maestro_aliasesses.nix
@@ -3,9 +3,11 @@
 let
   # Импортируем программы (передаем им pkgs)
   rebuild-wiz = import ./nix-commands/maestro_nix-commands.nix { inherit pkgs; };
+                       ./niri-commands/maestro_niri-commands.nix { inherit pkgs; };
   kori-menu   = import ./kori-menu.nix { inherit pkgs; };
 in
 {
+  
   # Собираем все импортированные программы в один мета-пакет
   icebook-aliasses-pkgs = pkgs.symlinkJoin {
     name = "icebook-aliasses-pkgs";
